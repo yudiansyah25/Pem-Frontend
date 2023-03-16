9d9b9ee1624c70e6706c42f008acba3cdb2aa2b2
yudiansyah25 <yudibbid225@gmail.com>
2023-03-16 21:37:43 +0700

    pertemuan-3

---
+++ src/data/users.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    name: "Yuda",
+    age: 22,
+    major: "Informatics",
+  },
+  {
+    name: "Raihan",
+    age: 20,
+    major: "Android Engineer",
+  },
+  {
+    name: "Agung",
+    age: 24,
+    major: "Data Engineer",
+  },
+  {
+    name: "Nurul",
+    age: 24,
+    major: "English",
+  },
+  {
+    name: "Jaki",
+    age: 27,
+    major: "English",
+  },
+];
+
+/**
+ * TODO 1.
+ * Export variable users.
+ * Gunakan export default.
+ */
+export default users;
