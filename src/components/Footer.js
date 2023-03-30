56324074d964e7e539927210f10dd16f95b04f04
yudiansyah25 <yudibbid225@gmail.com>
2023-03-30 20:17:47 +0700

    pertemuan-5

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <div class="footer">
+            <hr/>
+            <footer>
+                <h2>Copyright @divauswatun</h2>
+                <p>Created by React.js</p>
+            </footer>    
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+
+function Main() {
+    return (
+        <main>
+            <Hello nama="Adiva uswatun hasanah" jurusan="Teknik Informatika" gender="Cantik"/>
+            <Hello nama="Muhammad Yudiansyah" jurusan="Sistem Informasi" gender="Ganteng"/><br />
+        </main>
+    )
+}
+
+export default Main
